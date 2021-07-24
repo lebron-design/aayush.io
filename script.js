b4ac5a13268385d82e25e6c38bdc3c545f22c760
lebron-design <84165231+lebron-design@users.noreply.github.com>
2021-07-24 21:05:15 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.querySelector(".book").addEventListener("click", function () {
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".left-content").classList.add("white-color");
+  document.querySelector(".links-1").classList.add("white-color");
+  document.querySelector(".links-2").classList.add("white-color");
+  document.querySelector(".links-3").classList.add("white-color");
+  document.querySelector(".links-4").classList.add("white-color");
+  document.querySelector(".logo").classList.add("white-color");
+});
